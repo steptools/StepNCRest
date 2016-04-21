21ba5d559454e069d7a1093ef90da32879ceeca5
Samson <bonfante@steptools.com>
2016-04-21 15:02:00 -0400

    Added functions for accessing machinestate stuff

--- StepNCRest/Modules/StepModule.cs
+++ StepNCRest/Modules/StepModule.cs
@@ -32,8 +32,78 @@ namespace StepNCRest.Modules
                     });
             };
 
+            Get["/projects/{id}/geometry"] = parameters =>
+            {
+                return Mutexify((string)parameters.id,
+                    sti =>
+                    {
+                        return Response.AsText(sti.GetStateGeom());
+                    });
             };
 
+            Get["/projects/{pid}/geometry/{type}/{eid}"] = parameters =>
+            {
+                return Mutexify((string)parameters.pid,
+                    sti =>
+                    {
+                        STEPNCLib.MachineState.GeomType typ = STEPNCLib.MachineState.GeomType.NONE;
+                        if ((string)parameters.type == "shell")
+                            typ = STEPNCLib.MachineState.GeomType.MESH;
+                        else if ((string)parameters.type == "annotation")
+                            typ = STEPNCLib.MachineState.GeomType.POLYLINE;
+                        if (typ == STEPNCLib.MachineState.GeomType.NONE)
+                            return Response.AsText("");
+                        return Response.AsText(sti.GetStateGeom((string)parameters.eid, typ));
+                    });
+            };
+
+            Get["/projects/{id}/keystate"] = paramters =>
+            {
+                return Mutexify((string)paramters.id,
+                    sti =>
+                    {
+                        return Response.AsText(sti.GetKeyState());
+                    });
+            };
+
+            Get["/projects/{id}/deltastate"] = parameters =>
+            {
+                return Mutexify((string)parameters.id,
+                    sti =>
+                    {
+                        return Response.AsText(sti.GetDeltaState());
+                    });
+            };
+
+            Get["/projects/{id}/step"] = parameters =>
+            {
+                return Mutexify((string)parameters.id,
+                    sti =>
+                    {
+                        int resp = sti.StepState();
+                        if (resp == 0)
+                            return Response.AsText("OK");
+                        else if (resp == 1)
+                            return Response.AsText("SWITCH");
+                        else
+                            return Response.AsText("ERROR");
+                    });
+            };
+            Get["/projects/{id}/workingstep/{step}"] = parameters =>
+            {
+                return Mutexify((string)parameters.id,
+                    sti =>
+                    {
+                        int resp = -1;
+                        if ((string)parameters.step == "next")
+                            resp = sti.SetStateNextWS();
+                        else
+                            resp = sti.SetStateWS((uint)parameters.step);
+                        if (resp == 0)
+                            return Response.AsText("OK");
+                        else return Response.AsText("ERROR");
+                    });
+            };
         }
 
         public static StepInterface GetStepInterface(string id)
