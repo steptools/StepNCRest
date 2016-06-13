42c1b47af8c82ffe7cf4af06bf93e8dca39e5560
Kathryn Lovell <ununquadiumfiddle@gmail.com>
2016-06-13 08:28:31 -0400

    Added command line argument for port change

--- StepNCRest/Program.cs
+++ StepNCRest/Program.cs
@@ -7,10 +7,17 @@ namespace StepNCRest
     {
         static void Main(string[] args)
         {
-            var uri =
-                new Uri("http://127.0.0.1:8081");
-
-            using (var host = new NancyHost(uri))
+            Console.WriteLine("Choose a port to run your application. (Press [Enter] for default, Port 8081");
+            var port = Console.ReadLine();
+            var uri = new Uri("http://127.0.0.1:8081");
+            if (port != "")
+            {
+                uri =
+                    new Uri("http://127.0.0.1:" + port);
+            }
+            HostConfiguration config = new HostConfiguration();
+            config.UrlReservations.CreateAutomatically = true;
+            using (var host = new NancyHost(config,uri))
             {
                 host.Start();
 
